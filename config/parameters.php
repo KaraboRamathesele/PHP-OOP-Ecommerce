2e0ab48dcc3c9f24f0dc38f9fd1378bab96117e7
KaraboRamathesele <karaboramathesele8115@gmail.com>
2022-12-12 00:30:43 +0200

    setting Up User registration

---
+++ config/parameters.php
@@ -0,0 +1,6 @@
+<?php
+// The define() function defines a constant
+define("base_url", "http://localhost/Ecommerce%20PHP%20OOP/ecommerce-food-project-php/");
+//defalut is the productController
+define("default_controller", "productController");
+define("default_action", "index");
